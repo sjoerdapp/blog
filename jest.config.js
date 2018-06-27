ef4cfea78fe0e1aca125f6322929d074722f59e2
Jason Abbott <dev@trailimage.com>
2018-06-26 18:28:34 -0600

    initial setup

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,6 @@
-module.exports = require('@toba/test/jest');
+const config = require('@toba/test/jest');
+config.transform = {
+   '^.+\\.(ts|tsx)$':
+      '<rootDir>/node_modules/@stencil/core/testing/jest.preprocessor.js'
+};
+module.exports = config;
